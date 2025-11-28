b00d607b82572b2bade12ae29895c599e9ff4337
Dani3l <batistadaniel3007@gmail.com>
2025-11-28 16:28:10 +0000

    Added a status change function to the code,a filter to the tasks and changed the order of the code.

--- to-do-app/todo.py
+++ to-do-app/todo.py
@@ -1,4 +1,60 @@
 import flet as ft
+    
+class TodoApp(ft.Column): # Main To-Do App component
+    def __init__(self):
+        super().__init__()
+        self.new_task = ft.TextField(hint_text="Create a new task...", expand=True)
+        self.tasks = ft.Column()
+
+        self.filter = ft.Tabs( # Tabs for filtering tasks
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="All"), ft.Tab(text="Active"), ft.Tab(text="Completed")],
+        )
+
+        self.width = 600
+        self.controls = [ # Adds a plus button and the new task input field
+            ft.Row(
+                controls=[
+                    self.new_task,
+                    ft.FloatingActionButton(
+                        icon=ft.Icons.ADD, on_click=self.add_clicked
+                    ),
+                ],
+            ),
+            ft.Column(
+                spacing=25,
+                controls=[
+                    self.filter,
+                    self.tasks,
+                ]
+            )
+        ]
+
+    def add_clicked(self, e): # Function to add a new task
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+        self.tasks.controls.append(task)
+        self.new_task.value = ""
+        self.update()
+
+    def task_status_change(self): # Function to update task status
+        self.update()
+
+    def task_delete(self, task): # Function to delete a task
+        self.tasks.controls.remove(task)
+        self.update()
+
+    def before_update(self): # Function to filter tasks before updating the UI
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "All"
+                or (status == "Active" and not task.completed)
+                or (status == "Completed" and task.completed)
+            )
+
+    def tabs_changed(self, e): # Function to handle tab changes
+        self.update()
 
 class Task(ft.Column): # Task component
 
@@ -60,36 +116,12 @@ class Task(ft.Column): # Task component
         self.edit_view.visible = False
         self.update()
 
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change()
+
     def delete_clicked(self, e): # Function to delete the task
         self.task_delete(self)
-    
-class TodoApp(ft.Column): # Main To-Do App component
-    def __init__(self):
-        super().__init__()
-        self.new_task = ft.TextField(hint_text="Create a new task...", expand=True)
-        self.tasks = ft.Column()
-        self.width = 600
-        self.controls = [ # Adds a plus button and the new task input field
-            ft.Row(
-                controls=[
-                    self.new_task,
-                    ft.FloatingActionButton(
-                        icon=ft.Icons.ADD, on_click=self.add_clicked
-                    ),
-                ],
-            ),
-            self.tasks,
-        ]
-
-    def add_clicked(self, e): # Function to add a new task
-        task = Task(self.new_task.value, self.task_delete)
-        self.tasks.controls.append(task)
-        self.new_task.value = ""
-        self.update()
-
-    def task_delete(self, task): # Function to delete a task
-        self.tasks.controls.remove(task)
-        self.update()
 
 def main(page: ft.Page):
     page.title = "To-Do App"
