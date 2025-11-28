69ef9e4f7a7f4288468063f75e23bcb5b22c67f5
Dani3l <batistadaniel3007@gmail.com>
2025-11-28 10:43:22 +0000

    Centered all the content horizontally and now created tasks appear below the textfield

--- to-do-app/todo.py
+++ to-do-app/todo.py
@@ -1,31 +1,28 @@
-from asyncio import tasks
-from hmac import new
-from turtle import width
 import flet as ft
 
+
 def main(page: ft.Page):
-    def add_clicked(e): #adds a clickable checkbox with the task text
-        tasks_view.controls.append(ft.checkbox(label="new_task.value"))
+    def add_clicked(e): # Adds a new task to the list
+        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
         new_task.value = ""
         view.update()
-    
-    new_task = ft.TextField(hint_text="Create a new task...", expand=True)
+
+    new_task = ft.TextField(hint_text="What needs to be done?", expand=True) # Adds a text field for new tasks
     tasks_view = ft.Column()
-    view=ft.Column(
-        width= 600,
+    view=ft.Column( # Main page layout
+        width=600,
         controls=[
             ft.Row(
                 controls=[
                     new_task,
                     ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
                 ],
-            ), 
-            tasks_view,    
+            ),
+            tasks_view,
         ],
     )
 
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER # Centers all the content horizontally
     page.add(view)
-
 
 ft.app(main)
