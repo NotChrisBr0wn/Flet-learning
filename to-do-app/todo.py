e6513c8d32702aca23227b5a66deb33fe9f86e28
Dani3l <batistadaniel3007@gmail.com>
2025-11-28 16:44:15 +0000

    Fixed some errors

--- to-do-app/todo.py
+++ to-do-app/todo.py
@@ -1,5 +1,75 @@
 import flet as ft
+
+
+class Task(ft.Column): # Task component
+
+    def __init__(self, task_name, task_status_change, task_delete): # Initialize the task component
+        super().__init__()
+        self.completed = False
+        self.task_name = task_name
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
+        self.display_task = ft.Checkbox(value=False, label=self.task_name, on_change=self.status_changed)
+        self.edit_name = ft.TextField(expand=1)
+
+        self.display_view = ft.Row( 
+             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+             vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                controls=[
+                    self.display_task,
+                    ft.Row( 
+                         spacing = 0,
+                         controls = [
+                              ft.IconButton(
+                                icon=ft.Icons.CREATE_OUTLINED,
+                                tooltip="Edit task",
+                                on_click=self.edited_clicked,
+                              ),
+                              ft.IconButton(
+                                icon=ft.Icons.DELETE_OUTLINE,
+                                tooltip="Delete task",
+                                on_click=self.delete_clicked,
+                              ),
+                         ],
+                    ),
+                ],
+        )
+
+        self.edit_view = ft.Row(
+             visible = False,
+             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+             vertical_alignment=ft.CrossAxisAlignment.CENTER,
+             controls=[
+                 self.edit_name,
+                 ft.IconButton(
+                    icon=ft.Icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.Colors.GREEN,
+                    tooltip="Save changes",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        self.controls = [self.display_view, self.edit_view] 
     
+    def edited_clicked(self, e): # Function to switch to edit mode
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+    
+    def save_clicked(self, e): # Function to save the edited task name
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def status_changed(self, e): # Function to handle status change
+        self.completed = self.display_task.value
+        self.task_status_change()
+
+    def delete_clicked(self, e): # Function to delete the task
+        self.task_delete(self)
+
 class TodoApp(ft.Column): # Main To-Do App component
     def __init__(self):
         super().__init__()
@@ -55,73 +125,6 @@ class TodoApp(ft.Column): # Main To-Do App component
 
     def tabs_changed(self, e): # Function to handle tab changes
         self.update()
-
-class Task(ft.Column): # Task component
-
-    def __init__(self, task_name, task_delete): # Initialize the task with its name and delete function
-        super().__init__()
-        self.task_name = task_name
-        self.task_delete = task_delete
-        self.display_task = ft.Checkbox(value=False, label=self.task_name)
-        self.edit_name = ft.TextField(expand=1)
-
-        self.display_view = ft.Row( 
-             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-             vertical_alignment=ft.CrossAxisAlignment.CENTER,
-                controls=[
-                    self.display_task,
-                    ft.Row( 
-                         spacing = 0,
-                         controls = [
-                              ft.IconButton(
-                                icon=ft.Icons.CREATE_OUTLINED,
-                                tooltip="Edit task",
-                                on_click=self.edited_clicked,
-                              ),
-                              ft.IconButton(
-                                icon=ft.Icons.DELETE_OUTLINE,
-                                tooltip="Delete task",
-                                on_click=self.delete_clicked,
-                              ),
-                         ],
-                    ),
-                ],
-        )
-
-        self.edit_view = ft.Row(
-             visible = False,
-             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-             vertical_alignment=ft.CrossAxisAlignment.CENTER,
-             controls=[
-                 self.edit_name,
-                 ft.IconButton(
-                    icon=ft.Icons.DONE_OUTLINE_OUTLINED,
-                    icon_color=ft.Colors.GREEN,
-                    tooltip="Save changes",
-                    on_click=self.save_clicked,
-                ),
-            ],
-        )
-        self.controls = [self.display_view, self.edit_view] 
-    
-    def edited_clicked(self, e): # Function to switch to edit mode
-        self.edit_name.value = self.display_task.label
-        self.display_view.visible = False
-        self.edit_view.visible = True
-        self.update()
-    
-    def save_clicked(self, e): # Function to save the edited task name
-        self.display_task.label = self.edit_name.value
-        self.display_view.visible = True
-        self.edit_view.visible = False
-        self.update()
-
-    def status_changed(self, e):
-        self.completed = self.display_task.value
-        self.task_status_change()
-
-    def delete_clicked(self, e): # Function to delete the task
-        self.task_delete(self)
 
 def main(page: ft.Page):
     page.title = "To-Do App"
