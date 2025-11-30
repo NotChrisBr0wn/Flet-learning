e13734794e46c394bb683c801d2e62c7a8860620
Daniel BATISTA <batistadaniel3007@gmail.com>
2025-11-30 01:51:42 +0000

    Created a container for the layout and added classes for the operation buttons

--- calculator-app/calc.py
+++ calculator-app/calc.py
@@ -1,51 +1,88 @@
 import flet as ft
 
+class CalcButton(ft.ElevatedButton): # class for calculator buttons
+    def __init__(self,text, expand = 1):
+        super().__init__()
+        self.text = text
+        self.expand = expand
+        
+class DigitButton(CalcButton): # class for digit button
+    def __init__(self,text, expand = 1):
+        CalcButton.__init__(self,text, expand)
+        self.bgcolor = ft.Colors.WHITE24
+        self.color = ft.Colors.WHITE
+        
+class ActionButton(CalcButton): # class for action button
+    def __init__(self,text):
+        CalcButton.__init__(self,text)
+        self.bgcolor = ft.Colors.ORANGE
+        self.color = ft.Colors.WHITE
+        
+class ExtraActionButton(CalcButton): # class for extra action button
+    def __init__(self,text):
+        CalcButton.__init__(self,text)
+        self.bgcolor = ft.Colors.BLUE_GREY_100
+        self.color = ft.Colors.BLACK
+
 def main(page: ft.Page):
     page.title = "Calculator App"
-    result = ft.Text(value="0")
+    result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
     
-    page.add( # calculator layout
-        ft.Row(controls=[result]),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="AC"),
-                ft.ElevatedButton(text="+/-"),
-                ft.ElevatedButton(text="%"),
-                ft.ElevatedButton(text="/"),
-            ]
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="7"),
-                ft.ElevatedButton(text="8"),
-                ft.ElevatedButton(text="9"),
-                ft.ElevatedButton(text="*"),
-            ]
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="4"),
-                ft.ElevatedButton(text="5"),
-                ft.ElevatedButton(text="6"),
-                ft.ElevatedButton(text="-"),
-            ]
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="1"),
-                ft.ElevatedButton(text="2"),
-                ft.ElevatedButton(text="3"),
-                ft.ElevatedButton(text="+"),
-            ]
-        ),
-        ft.Row(
-             controls=[
-                ft.ElevatedButton(text="0"),
-                ft.ElevatedButton(text="."),
-                ft.ElevatedButton(text="="),
-            ]
-        ),
-    )
-
+    page.add(
+        ft.Container(
+            width=350,
+            bgcolor=ft.Colors.BLACK,
+            border_radius=ft.border_radius.all(20),
+            padding=20,
+            content=ft.Column(
+                controls=[
+                    ft.Row(controls=[result], alignment="end"),
+                    ft.Row(
+                        controls=[
+                            ExtraActionButton(text="AC"),
+                            ExtraActionButton(text="+/-"),
+                            ExtraActionButton(text="%"),
+                            ActionButton(text="/"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(text="7"),
+                            DigitButton(text="8"),
+                            DigitButton(text="9"),
+                            ActionButton(text="*"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(text="4"),
+                            DigitButton(text="5"),
+                            DigitButton(text="6"),
+                            ActionButton(text="-"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(text="1"),
+                            DigitButton(text="2"),
+                            DigitButton(text="3"),
+                            ActionButton(text="+"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(text="0", expand=2),
+                            DigitButton(text="."),
+                            ActionButton(text="="),
+                        ]
+                    ),
+                ]
+            ),
+                    # Controls will the six rows with the text 
+                       # and the calculator buttons.
+            )
+        )
+        
+    
     
 ft.app(main)
